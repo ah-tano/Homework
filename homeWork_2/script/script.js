97437e64a91dc60483fcee003245898f39ccaa9f
Nastiia <lanastasialysenkol@gmail.com>
2020-03-22 15:09:53 +0200

    Homework divided into sub-folders. Homework 2 added.

---
+++ homeWork_2/script/script.js
@@ -0,0 +1,186 @@
+const logItems = function(array) {
+    for (let i = 0; i < array.length; i++) {
+        console.log(`${i + 1} - ${array[i]}`)
+    }
+};
+
+logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+
+logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+
+
+
+const calculateEngravingPrice = function(message, pricePerWord) {
+    const wordsArray = Array.from(message.split(' '));
+    let total;
+    total = wordsArray.length * pricePerWord;
+    return total; 
+};
+
+console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 10));
+console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 20));
+console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40));
+console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20));
+
+
+
+const findLongestWord = function(string) {
+    const wordsArray = Array.from(string.split(' '));
+    let longestWord = wordsArray[0];
+
+    for(let word of wordsArray) {
+
+        if (word.length > longestWord.length) {
+
+            longestWord = word;
+        }  
+
+    }
+
+    return longestWord;
+};
+
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+console.log(findLongestWord('Google do a roll'));
+console.log(findLongestWord('May the force be with you'));
+
+
+
+const formatString = function(string) {
+
+    if (string.length > 40) {
+        return string.slice(0, 41) + '...';
+    }
+
+    return string;
+};
+
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+console.log(formatString('Curabitur ligula sapien.'));
+console.log(formatString('Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.'));
+
+
+
+const checkForSpam = function(message) {
+    message = message.toLowerCase();
+
+    const spamWords = ['spam', 'sale'];
+
+    for(let word of spamWords) {
+
+        if (message.includes(word)) {
+            return true;
+        }
+    }
+
+    return false;
+};  
+
+console.log(checkForSpam('Latest technology news'));
+console.log(checkForSpam('JavaScript weekly newsletter'));
+console.log(checkForSpam('Get best sale offers now!'));
+console.log(checkForSpam('[SPAM] How to earn fast money?'));
+
+
+
+
+const showSum = function(numbers = [], input = prompt('Please, enter the number')) {
+
+    if (isNaN(input)) {
+        alert('Было введено не число, попробуйте еще раз');
+        showSum();
+    };
+
+    if (input !== null) {
+
+        numbers.push(Number(input));
+        showSum(numbers);
+
+    } else {
+
+        let total = 0;
+
+        for(let number of numbers) {
+            total += number;
+        }
+
+        return console.log(total);
+    }
+
+
+};
+
+showSum();
+    
+
+let addNewUser = function() {
+
+    const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+
+    let userLogin;
+
+    const getLogin =  function() { 
+        return userLogin = prompt('Enter your login');
+    }
+
+
+    const isLoginValid = function(login) {
+        
+        if (login.length < 4 || login.length > 16) {
+            return false; 
+        } else { 
+            return true;
+        }
+
+    }
+
+
+    const isLoginUnique = function(logins, login) {
+
+        if (logins.includes(login)) {
+            return false;
+        };
+
+        return true;
+
+    }
+
+
+    const addLogin = function(logins, login) {
+        
+        let message;
+
+        if (isLoginValid(userLogin) === false) {
+
+            message = 'Ошибка! Логин должен быть от 4 до 16 символов';
+            return console.log(message);
+        
+        } 
+
+        if (isLoginUnique(logins, userLogin) === false) {
+            
+            message = 'Такой логин уже используется!';
+            return console.log(message);
+
+        } 
+
+            logins.push(userLogin);
+            message = 'Логин успешно добавлен!';
+            console.log(logins);
+            return console.log(message);
+    }
+
+
+    addLogin(logins, getLogin());
+}
+
+addNewUser();
+
+
+
+
+
+
+
+
