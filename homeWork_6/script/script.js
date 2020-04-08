025189dc7cf6a9dbd88d69a07e26d9b3b64f615b
Nastiia <lanastasialysenkol@gmail.com>
2020-04-08 18:12:18 +0300

    Homework 6 updated

--- homeWork_6/script/script.js
+++ homeWork_6/script/script.js
@@ -24,9 +24,11 @@ console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley
 * Получить массив имен пользователей по полу (поле gender).
 */
 
-const getUsersWithGender = (users, gender) => users.filter(user => user.gender === gender);
+const getUsersWithGender = (users, gender) => users
+.filter(user => user.gender === gender)
+.map(user => user.name)
   
-  console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
 /*
 *  TASK 4
@@ -54,13 +56,14 @@ console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект п
 
 const getUsersWithAge = (users, min, max) => users.filter(user => user.age >= min && user.age <= max);
 
-console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+console.log(getUsersWithAge(users, 20, 30)); 
+// [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
 
 console.log(getUsersWithAge(users, 30, 40));
 // [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
 
 /*
-*  TASK 6
+*  TASK 7
 * Получить общую сумму баланса (поле balance) всех пользователей.
 */
 
@@ -69,26 +72,44 @@ const calculateTotalBalance = users => users.reduce((totalBalance, user) => tota
 console.log(calculateTotalBalance(users)); // 20916
 
 /*
-*  TASK 7
+*  TASK 8
 * Массив имен всех пользователей у которых есть друг с указанным именем.
 */
-//перебрать friends
-//if true push
+
+const getUsersWithFriend = (users, friendName)  => users
+  .filter(user =>
+    user.friends.find(friend => friend === friendName))
+  .map(user => user.name)
 
 
-const getUsersWithFriend = (users, friendName) => users.reduce((allFriends, user) => {
-    allFriends.push(...user.friends);
+console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
 
-    return allFriends;
-}, []);
+/*
+*  TASK 9
+* Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
+*/
 
-console.log(getUsersWithFriend(users));
+const getNamesSortedByFriendsCount = users => users
+.sort( (user1, user2) => user1.friends.length - user2.friends.length)
+.map(user  => user.name)
 
+console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
 
+/*
+*  TASK 10
+* Получить массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
+*/
 
+const getSortedUniqueSkills = users => users
+.reduce((allSkills, user) => { 
+  allSkills.push(...user.skills);
 
+  return allSkills;
+}, [])
+.filter((skill, index, allSkills) => allSkills.indexOf(skill) === index)
+.sort()
 
-
-
-// console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
-// console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
